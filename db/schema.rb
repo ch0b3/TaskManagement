f2d534f385069b336e61b53ba537cdbf58d1a844
大坂屋 賢志 <1111913@PC0545.local>
2019-08-05 12:53:58 +0900

    create-status

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_025901) do
+ActiveRecord::Schema.define(version: 2019_08_05_032853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "priorities", force: :cascade do |t|
     t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
