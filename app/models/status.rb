18d2725b977b0eb1f814ae6381bd54243c08472f
大坂屋 賢志 <1111913@PC0545.local>
2019-08-09 17:05:38 +0900

    create-validation

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,6 @@
 class Status < ApplicationRecord
   has_many :tasks
+  validates_associated :tasks
 
   validates :name, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,18 @@ class Task < ApplicationRecord
   belongs_to :priority
   belongs_to :status_id, :class_name => "Status", foreign_key: "status"
 
-  validates :name, presence: true
-  
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, presence: true, length: { maximum: 100 }
+  validates :deadline, presence: true
+  validates :status, presence: true, numericality: { only_integer: true }
+  # validates :user_id, presence: true
+  validates :priority_id, presence: true, numericality: { only_integer: true }
+  validate :not_before_today
+
+  private
+
+  def not_before_today
+    errors.add(:deadline, 'が正しくありません') if deadline < Date.today
+  end
+
 end
