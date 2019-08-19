8a731452f7cadac4635d242227c364c15e55c4b5
大坂屋 賢志 <1111913@PC0545.local>
2019-08-19 17:24:15 +0900

    modify callback

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,9 @@ class Task < ApplicationRecord
   belongs_to :priority
 
   belongs_to :status_id, :class_name => "Status", foreign_key: "status"
-  
-  
-  
+
+
+
   before_validation :set_default_status
 
   private
