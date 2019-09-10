92037c61fd946f039e03619c7f71faa76c07abc5
大坂屋 賢志 <1111913@PC0545.local>
2019-09-10 18:57:18 +0900

    modify factorybot

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,7 +3,5 @@ FactoryBot.define do
     sequence(:name) {|n| "タスク#{n}"}
     description { "説明文" }
     deadline { "2020-04-01" }
-    status  { [1,2,3].sample }
-    priority_id { [1,2,3].sample } 
   end
 end
