f3045b5a4255ef9ab2d8bafb8ee13d6f45d9d86a
大坂屋 賢志 <1111913@PC0545.local>
2020-04-06 18:20:24 +0900

    adminのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
+
   root to: "tasks#index"
   resources :tasks
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
