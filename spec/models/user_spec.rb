0681099a0cebb740fe426c9c361caf01d48043d7
大坂屋 賢志 <1111913@PC0545.local>
2020-04-06 17:37:19 +0900

    userのバリデーションの修正とspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate' do
+    subject { FactoryBot.build(:user, params) }
+    let(:params) {
+      {
+        name: name,
+        email: email,
+        password: password
+      }
+    }
+    context "with valid params" do
+      where(:name, :email, :password) do
+        [
+          ['name', 'email@gmail.com', 'password']
+        ]
+      end
+      with_them do
+        it { is_expected.to be_valid }
+      end
+    end
+    context "with invalid params" do
+      context "with empty params" do
+        where(:name, :email, :password) do
+          [
+            ['', 'email@gmail.com', 'password'],
+            ['name', '', 'password'],
+            ['name', 'email@gmail.com', '']
+          ]
+        end
+        with_them do
+          it { is_expected.to be_invalid }
+        end
+      end
+      context "password is less than 7 characters" do
+        where(:name, :email, :password) do
+          [
+            ['name','email@gmail.com','a']
+          ]
+        end
+        with_them do
+          it { is_expected.to be_invalid }
+        end
+      end
+      context "name or email has too many characters" do
+        where(:name, :email, :password) do
+          [
+            ['123456789123456789123456789123456789', 'email@gmail.com', 'password'],
+            ['name', '123456789123456789123456789123456789123456789', 'password']
+          ]
+        end
+        with_them do
+          it { is_expected.to be_invalid }
+        end
+      end
+    end
+  end
 end
