f59e69871ccd49406a46e713c6e21a018af9a724
大坂屋 賢志 <1111913@PC0545.local>
2020-04-07 20:08:43 +0900

    管理者がいなくならないようにコールバック

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum:40 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
+
+  before_destroy :check_admin_user_exist
+
+  private
+
+  def check_admin_user_exist
+    if self.admin?
+      throw :abort if User.where(admin: true).count == 1
+    end
+  end
 end
